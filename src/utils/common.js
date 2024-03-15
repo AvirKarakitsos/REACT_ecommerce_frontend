d8db1958d7915aa572beb539e20b8d7cdfe900a2
Avir Karakitsos <avir.karakitsos@gmail.com>
2024-03-15 10:46:14 +0100

    feat: addZero function

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,6 +1,13 @@
+const addZero = (number) => {
+    if(number<10) return "0"+number
+    else return number
+}
+
 export const formatPrice = (price) => (price/100).toFixed(2)+"€"
+
 
 export const formatDate = (timestamp) => {
     const date = new Date(parseInt(timestamp))
-    return date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()+" à "+date.getHours()+":"+date.getMinutes()
-} 
+    return date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()+" à "+date.getHours()+":"+addZero(date.getMinutes())
+}
+
