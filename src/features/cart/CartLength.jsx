eb397629fb8d1f1485feb9a016b7cfc592871536
Avir Karakitsos <avir.karakitsos@gmail.com>
2024-03-22 14:55:37 +0100

    feat: style for CartLenght component

--- src/features/cart/CartLength.jsx
+++ src/features/cart/CartLength.jsx
@@ -1,12 +1,28 @@
 import { useSelector } from "react-redux"
 import { cartLength } from "./cartSlice"
+import styled from "styled-components"
+
+const Border = styled.span`
+    position: absolute;
+    top: -12px;
+    left: -12px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 23px;
+    width: 23px;
+    font-weight: 400;
+    border-radius: 50%;
+    background-color: blue;
+    color: white;
+`
 
 
 function CartLength() {
     const counter = useSelector(cartLength)
 
     return (
-        <span>{counter}</span>
+        <Border>{counter}</Border>
     )
 }
 
